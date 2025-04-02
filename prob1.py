34184e4db02bcd9e9cd535bf79005550e237ead1
Justin Nguyen <iamaalt921@gmail.com>
2025-04-01 17:39:18 -0700

    Fixed Prob 1 since I misread the question

--- prob1.py
+++ prob1.py
@@ -1,19 +1,6 @@
-threeMulti = []
-fiveMulti = []
-threeSum = 0
-fiveSum = 0
+sum = 0
 for i in range(1, 1000):
+    if i % 5 == 0 or i % 3 == 0:
+        sum += i
 
-    threeAdd = i * 3
-    fiveAdd = i * 5
-    if threeAdd < 1000:
-        threeMulti.append(i * 3)
-    if fiveAdd < 1000:
-        fiveMulti.append(fiveAdd)
-
-for i in range(len(threeMulti)):
-    threeSum += threeMulti[i]
-for i in range(len(fiveMulti)):
-    fiveSum += fiveMulti[i]
-
-print(threeSum+fiveSum)
+print(sum)
