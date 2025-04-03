14bb4f9819726976e64a7da6df5d6f781c60751b
Justin <iamaalt921@gmail.com>
2025-04-03 16:50:42 -0700

    Deleted the code isPrime code because the wikipedia provides a much more diffucult implementation

--- scripts/aksPrime.cpp
+++ scripts/aksPrime.cpp
@@ -1,58 +1,19 @@
 #include <math.h>
 #include <iostream>
 #include <numeric>
-
-int coprime(int a, int b){
-    int coprime = 0;
-
-    if (std::gcd(a, b) == 1){
-        coprime = 1;
-    }
-
-    return coprime;
-}
+#include <vector>
 
 bool isPrime(int n){
-    //Step 1: Determine if n is a perfect power, if so it is a composite
-    double power{0.0};
-    int r{2};
-    bool isPrime = true;
-    for (int i = 2; i <= log2(n); i++){
-        power = pow(n, 1.0/i);
-        if (((int)(power * 10)) % 10 == 0){
-            isPrime = false;
-            break;
-        }
-    }
-
-    //Step 2: Determine the smallest multiplicative order of n, a, and if a and n are NOT coprime it is a composite
-
-    if(isPrime == false){
-        return isPrime;
-    }
-
-    while (true){
-        int k = pow(log2(n), 2.0);
-        while (coprime(n, r) == 1){
-            if ((int(pow(n, k)) % r) == 1 && k > pow(log2(n), 2.0)){
-                goto end_loops;
-            }
-            k++;
-        }
-        r++;
-    }
-
-    end_loops:
-    if (coprime(r, n) != true){
-        isPrime = false;
-        return false;
-    }
+    //Equation: (x-1)^p = (x^p - 1)
+    bool isPrime = false;
+    
+    
 
     return isPrime;
 }
 
 int main(){
-    int n = 6;
+    int n = 31;
 
     if (isPrime(n) == 0){
         std::cout << n << " is not a prime";
