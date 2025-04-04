18a09d8f975a6684e76468ff3c4463892d77868b
Justin <iamaalt921@gmail.com>
2025-04-04 16:08:37 -0700

    Added random numbers

--- scripts/fermatsPrimeTest.cpp
+++ scripts/fermatsPrimeTest.cpp
@@ -2,4 +2,25 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include<windows.h>
 
+
+bool fermatPrimality(long long n){
+
+    bool isPrime = false;
+    long long randNum = (rand() % (n-1)) + 2;
+
+    for (int i = 0; i < 10; i++){
+        if (pow(randNum, n-1) % n == 1){
+
+        }
+    }
+
+    return isPrime;
+}
+
+int main(){
+    for (int i = 0; i < 10; i++){
+    std::cout << rand() << "\n";
+    }
+}
