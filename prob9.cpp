70861b543e333304ed4f361417f70766992bda04
Justin Nguyen <iamaalt921@gmail.com>
2025-04-07 19:49:03 -0700

    Turns out my answer was right all a long. I just used break wrong. Also I get theses werid errors about cout not being recognized. idk

--- prob9.cpp
+++ prob9.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 int main(){
     //a^2 + b^2 = c^2
     //a<b<c
     // a + b + c = 1000
-    int a{0};
-    int b{0};
+    double a{1};
+    double b{1};
     double c{0};
+    long long result;
 
-    for (a; a < 998; a++){
+    for(a; a < 998; a++){
         for (b = a+1; b < 998; b++){
             c = sqrt(pow(a, 2) + pow(b, 2));
             ///std::cout << a << " " << b << " " << c << "\n";
             if (c - (int)c == 0.0 && c < 999){
-                std::cout << c << "\n";
                 if(a + b + (int)c == 1000){
-                    break;
+                    goto end_loops;
                 }
             }
         }
     }
 
-    std::cout << "a: " << a << " " << "b: " << b;
+    end_loops:
+    result = a * b * c;
+    std::cout << result;
     return 0;
 }
