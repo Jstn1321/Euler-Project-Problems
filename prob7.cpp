3854a891560a0030edcf260009fade97208024f8
Justin <iamaalt921@gmail.com>
2025-04-09 08:05:59 -0700

    Added my fermat's primality test algorithm, it works and gives me the right answer but still is slow

--- prob7.cpp
+++ prob7.cpp
@@ -2,26 +2,56 @@
 #include <iostream>
 #include <math.h>
 
-bool isMaybePrime(unsigned long long n){
-    mpz_t bigint;
-    mpz_init_set_si(bigint, n);
-    bool ret = mpz_probab_prime_p(bigint, 25) > 0;
-    mpz_clear(bigint);
-    return ret;
+#include <iostream>
+#include <math.h>
+#include <random>
+#include <gmpxx.h>
+#include <cstdlib>
+
+bool probablePrime(mpz_class n, unsigned long long longn){
+    bool isPrime = true;
+    mpz_class randNum{0};
+    mpz_class exp{0};
+    mpz_class mod{0};
+
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(2, longn-2);
+
+    for (int i = 0; i < 3; i++){
+        randNum = distrib(gen);
+        //std::cout << randNum << "\n";
+        //exp = pow(randNum, longn - 1);
+        mpz_pow_ui(exp.get_mpz_t(), randNum.get_mpz_t(), longn-1);
+        //std::cout << exp << "\n";
+        mod = exp % n;
+        //std::cout << mod << "\n";
+        if (mod != 1){
+            isPrime = false;
+            break;
+        }
+    }
+
+    return isPrime;
 }
 
 int main(){
-    int primeCount{1};
-    unsigned long long n{3};
+    int primeCount{2};
+    unsigned long long longn{5};
+    mpz_class n{5};
     while(true){
-        if (primeCount == 10001){
+        std::cout << n << " " << longn << " " << primeCount << "\n";
+        if (primeCount == 3){
             break;
         }
-        if (isMaybePrime(n)){
+        if (probablePrime(n, longn)){
             primeCount++;
         }
         n += 2;
+        longn += 2;
     }
-    n -= 2;
+    n-=2;
+    longn-=2;
     std::cout << n;
+    return 0;
 }
