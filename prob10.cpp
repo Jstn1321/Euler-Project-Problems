3d2ad80629525468f5ef3c18df34656cd0f66932
Justin Nguyen <iamaalt921@gmail.com>
2025-04-08 21:06:30 -0700

    Added experimental code, dosen't work though

--- prob10.cpp
+++ prob10.cpp
@@ -2,24 +2,29 @@
 #include <math.h>
 #include <random>
 #include <gmpxx.h>
-#include <string>
+#include <cstdlib>
 
 bool probablePrime(mpz_class n, unsigned long long longn){
     bool isPrime = true;
-    unsigned long long randNum{0};
+    mpz_class randNum{0};
     mpz_class exp{0};
+    mpz_class mod{0};
 
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(2,longn-2);
-
-    randNum = dist6(rng);
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(2, longn-2);
 
     for (int i = 0; i < 10; i++){
-        randNum = dist6(rng);
-        exp = pow(randNum, longn - 1);
-        if (exp % n != 1){
+        randNum = distrib(gen);
+        //std::cout << randNum << "\n";
+        //exp = pow(randNum, longn - 1);
+        mpz_pow_ui(exp.get_mpz_t(), randNum.get_mpz_t(), longn-1);
+        //std::cout << exp << "\n";
+        mod = exp % n;
+        //std::cout << mod << "\n";
+        if (mod != 1){
             isPrime = false;
+            break;
         }
     }
 
@@ -27,16 +32,16 @@ bool probablePrime(mpz_class n, unsigned long long longn){
 }
 
 int main(){
-    unsigned long long sum{1};
+    mpz_class sum{2};
     mpz_class mpzi{3};
 
-    for (unsigned long long i = 3; i < 2000000; i+=2){
-        if (probablePrime(mpzi,i) == 1){
-            sum += i;
+    for (long long i = 3; i < 10; i+=2){
+        std::cout << i << " " << sum << "\n";
+        if (probablePrime(mpzi, i) == 1){
+            sum = sum + mpzi;
         }
         mpzi++;
     }
 
-    std::string sumstr = std::stoi(sum);
     return 0;
 }
