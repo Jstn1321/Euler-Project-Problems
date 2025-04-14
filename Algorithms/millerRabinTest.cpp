67b17821034ea0efff76bb3b485ff49e33e48502
Justin Nguyen <iamaalt921@gmail.com>
2025-04-13 17:46:51 -0700

    Made miller rabin cpp file to try and implement as a faster prime test

---
+++ Algorithms/millerRabinTest.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <iostream>
+#include <gmpxx.h>
+
+
+bool probablePrime(mpz_class n, unsigned long long longn){
+    bool isPrime = false;
+    return isPrime;
+}
+
+
+int main(){
+    return 0;
+}
