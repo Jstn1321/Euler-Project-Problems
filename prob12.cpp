d2edbdab13b309c1c9812e807f9643b5c5bb5d6e
Justin Nguyen <iamaalt921@gmail.com>
2025-05-25 20:44:46 -0700

    Added function for the number of factors and the logic to solve the problem

--- prob12.cpp
+++ prob12.cpp
@@ -12,6 +12,31 @@ int triangleNumGen(int num)
     return triNum;
 }
 
+int numFactors(int num)
+{
+    int numFact{0};
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            numFact++;
+        }
+    }
+    return numFact;
+}
+
 int main()
 {
+    int num = 1;
+    while (true)
+    {
+        if (numFactors(triangleNumGen(num)) >= 500)
+        {
+            break;
+        }
+        std::cout << triangleNumGen(num) << "\n";
+        num++;
+    }
+
+    std::cout << triangleNumGen(num);
 }
